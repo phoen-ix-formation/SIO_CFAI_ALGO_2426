a6084a9ec82ff4b79e09be55946989d20cfaf6ae
karhel <karheltmarr@hotmail.fr>
2026-02-13 11:32:43 +0100

    Placement pacman sur le plateau

--- js_pacman/script.js
+++ js_pacman/script.js
@@ -2,11 +2,5 @@ const elGameDiv = document.getElementById('game');
 
 document.addEventListener('DOMContentLoaded', () => {
 
-    // Ajout de div dans la grille HTML pour faire apparaitre les cases
-    for(i = 0; i < 31; i++) {
-        for(j = 0; j < 28; j++) {
-
-            elGameDiv.appendChild(document.createElement('div'));
-        }
-    }
+    
 });
